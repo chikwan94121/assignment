2c8b8d10194b3fd0c8cb893a375722854703ba85
TANG Chi Kwan <s1096776@studentdmn.ouhk.edu.hk>
2015-12-04 17:08:30 +0800

    first test case

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ app.delete('/restaurant_id/:id',function(req,res) {
 			}
        		//console.log('Restaurant removed!')
        		db.close();
-			res.status(200).json({message: 'delete done', id: req.params.id});
+			res.status(200).json({message: 'delete done', restaurant_id: req.params.id});
     	});
     });
 });
@@ -78,9 +78,44 @@ app.get('/restaurant_id/:id', function(req,res) {
 				res.status(200).json(results);
 			}
 			else {
-				res.status(200).json({message: 'No matching document'});
+				res.status(200).json({message: 'No matching document', restaurant_id: req.params.id});
 			}
 			db.close();
+    	});
+    });
+});
+
+app.post('/restaurant_id/:id/grade',function(req,res) {
+	//console.log(req.body);
+	var restaurantSchema = require('./models/restaurant');
+	mongoose.connect(mongodbURL);
+	var db = mongoose.connection;
+	db.on('error', console.error.bind(console, 'connection error:'));
+	db.once('open', function (callback) {
+		var rObj = {};
+		rObj.address = {};
+		rObj.address.building = req.body.building;
+		rObj.address.street = req.body.street;
+		rObj.address.zipcode = req.body.zipcode;
+		rObj.address.coord = [];
+		rObj.address.coord.push(req.body.lon);
+		rObj.address.coord.push(req.body.lat);
+		rObj.borough = req.body.borough;
+		rObj.cuisine = req.body.cuisine;
+		rObj.name = req.body.name;
+		rObj.restaurant_id = req.body.restaurant_id;
+
+		var Restaurant = mongoose.model('Restaurant', restaurantSchema);
+		var r = new Restaurant(rObj);
+		//console.log(r);
+		r.save(function(err) {
+       		if (err) {
+				res.status(500).json(err);
+				throw err
+			}
+       		//console.log('Restaurant created!')
+       		db.close();
+			res.status(200).json({message: 'insert done', id: r._id});
     	});
     });
 });
