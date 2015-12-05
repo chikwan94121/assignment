6f77534edc6778c6edc21f4450bb2515590d11c1
developer <you@example.com>
2015-12-05 17:16:02 +0800

    Put function

--- server.js
+++ server.js
@@ -85,39 +85,29 @@ app.get('/restaurant_id/:id', function(req,res) {
     });
 });
 
-app.post('/restaurant_id/:id/grade',function(req,res) {
-	//console.log(req.body);
-	var restaurantSchema = require('./models/restaurant');
+app.put('/restaurant_id/:id/:attrib/:attrib_value', function(req,res) {
+	var criteria = {};
+	criteria[req.params.attrib] = req.params.attrib_value;
+
+	console.log(criteria);
+	
 	mongoose.connect(mongodbURL);
 	var db = mongoose.connection;
 	db.on('error', console.error.bind(console, 'connection error:'));
 	db.once('open', function (callback) {
-		var rObj = {};
-		rObj.address = {};
-		rObj.address.building = req.body.building;
-		rObj.address.street = req.body.street;
-		rObj.address.zipcode = req.body.zipcode;
-		rObj.address.coord = [];
-		rObj.address.coord.push(req.body.lon);
-		rObj.address.coord.push(req.body.lat);
-		rObj.borough = req.body.borough;
-		rObj.cuisine = req.body.cuisine;
-		rObj.name = req.body.name;
-		rObj.restaurant_id = req.body.restaurant_id;
-
 		var Restaurant = mongoose.model('Restaurant', restaurantSchema);
-		var r = new Restaurant(rObj);
-		//console.log(r);
-		r.save(function(err) {
-       		if (err) {
+		Restaurant.update({restaurant_id: req.params.id},{$set:criteria},function(err){
+			if (err) {
 				res.status(500).json(err);
 				throw err
 			}
-       		//console.log('Restaurant created!')
-       		db.close();
-			res.status(200).json({message: 'insert done', id: r._id});
-    	});
-    });
+				res.status(200).json(results);
+				db.close();
+				
+			}
+		});
+	});
 });
 
 app.listen(process.env.PORT || 8099);
+
