70513bafe3a78dcb481f7acfae5c32ece18a3b01
chikwan94121 <s1096776@ouhk.edu.hk>
2015-12-07 11:43:53 +0800

    Update server.js

--- server.js
+++ server.js
@@ -8,40 +8,7 @@ app.use(bodyParser.json());
 var mongodbURL = 'mongodb://MongoLab-w:XBVAFA8OxsKro.sTrBKxYczt2zKXtWRBTWQ3E2SC9Qs-@ds052968.mongolab.com:52968/MongoLab-w';
 var mongoose = require('mongoose');
 
-app.post('/',function(req,res) {
-	//console.log(req.body);
-	var restaurantSchema = require('./models/restaurant');
-	mongoose.connect(mongodbURL);
-	var db = mongoose.connection;
-	db.on('error', console.error.bind(console, 'connection error:'));
-	db.once('open', function (callback) {
-		var rObj = {};
-		rObj.address = {};
-		rObj.address.building = req.body.building;
-		rObj.address.street = req.body.street;
-		rObj.address.zipcode = req.body.zipcode;
-		rObj.address.coord = [];
-		rObj.address.coord.push(req.body.lon);
-		rObj.address.coord.push(req.body.lat);
-		rObj.borough = req.body.borough;
-		rObj.cuisine = req.body.cuisine;
-		rObj.name = req.body.name;
-		rObj.restaurant_id = req.body.restaurant_id;
 
-		var Restaurant = mongoose.model('Restaurant', restaurantSchema);
-		var r = new Restaurant(rObj);
-		//console.log(r);
-		r.save(function(err) {
-       		if (err) {
-				res.status(500).json(err);
-				throw err
-			}
-       		//console.log('Restaurant created!')
-       		db.close();
-			res.status(200).json({message: 'insert done', id: r._id});
-    	});
-    });
-});
 
 app.post('/',function(req,res) {
 	//console.log(req.body);
